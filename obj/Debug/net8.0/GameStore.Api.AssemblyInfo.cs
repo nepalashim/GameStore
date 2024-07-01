8250312ed09f2ba08d68d0f2928d1e5e8cb898e4
nepalashim <078bct020.ashim@pcampus.edu.np>
2024-07-01 08:11:11 +0545

    Implented Asynchronous programming model

--- obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49808841676725c95b31887919a1c2cf6046003f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b968e84f1da69b35eaa378ea19c9c72bbc3d296")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
